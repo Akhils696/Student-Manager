c47e41d68bc4877031847582602c565b47d9b538
Akhil Senthil <akhilsenthil696@gmail.com>
2026-02-28 22:05:00 +0530

    feat: update task routes with additional endpoints

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,5 +1,17 @@
 const express = require('express');
-const { getTasks, getTaskById, createTask, updateTask, deleteTask, getTasksByStudent } = require('../controllers/taskController');
+const { 
+  getTasks, 
+  getTaskById, 
+  createTask, 
+  updateTask, 
+  deleteTask, 
+  getTasksByStudent,
+  filterTasks,
+  getTaskStats,
+  bulkCreateTasks,
+  bulkUpdateTasks,
+  bulkDeleteTasks
+} = require('../controllers/taskController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
@@ -13,5 +25,10 @@ router.route('/:id')
   .delete(protect, deleteTask);
 
 router.get('/student/:studentId', protect, getTasksByStudent);
+router.get('/filter', protect, filterTasks);
+router.get('/stats', protect, getTaskStats);
+router.post('/bulk', protect, bulkCreateTasks);
+router.put('/bulk', protect, bulkUpdateTasks);
+router.delete('/bulk', protect, bulkDeleteTasks);
 
 module.exports = router;
